9f6d6879e5f67e4d6f31687a86d2043d8d6b5d99
Paul Gribble <pgribble@uwo.ca>
2022-05-18 20:10:16 -0400

    misc

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 #first install anaconda
-conda create --name motornet python=3.8
-conda activate motornet
+#then conda create --name motornet python=3.8
+#then conda activate motornet
+
 pip3 install --upgrade pip
 conda install numpy -y
 conda install -c apple tensorflow-deps -y
 pip3 install tensorflow-macos
 conda install pandas matplotlib scipy scikit-learn jupyter notebook -y
+
+
+# to remove: 
+# conda env remove --name motornet
