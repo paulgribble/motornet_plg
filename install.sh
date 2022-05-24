99ff497aaa8a27d269e764c1c0026d457a4a629c
Paul Gribble <pgribble@uwo.ca>
2022-05-24 19:47:54 -0400

    install

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ pip3 install --upgrade pip
 conda install numpy -y
 conda install -c apple tensorflow-deps -y
 pip3 install tensorflow-macos
-conda install pandas matplotlib scipy scikit-learn jupyter notebook -y
+conda install pandas matplotlib scipy scikit-learn jupyter ipython ipykernel notebook -y
 
 
 # to remove: 
