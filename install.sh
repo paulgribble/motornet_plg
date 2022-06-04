a8063ba52b86ce56edbdf9ecd6f7f9af3e148c2e
Paul Gribble <pgribble@uwo.ca>
2022-06-04 09:34:39 -0400

    install without anaconda

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# without relying on anaconda:
+
+python3 -m venv motornet_env
+source motornet_env/bin/activate
+cd motornet_env/
+python -m pip install -U pip
+pip install tensorflow-macos
+# pip install tensorflow-metal # for GPU-based stuff
+
+
+
+
 #first install anaconda
 #then conda create --name motornet python=3.8
 #then conda activate motornet
@@ -9,7 +21,6 @@ conda install numpy -y
 conda install -c apple tensorflow-deps -y
 pip3 install tensorflow-macos
 conda install pandas matplotlib scipy scikit-learn jupyter ipython ipykernel notebook -y
-
 
 # to remove: 
 # conda env remove --name motornet
